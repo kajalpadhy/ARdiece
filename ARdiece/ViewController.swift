556d09eeed34aeb92612c2d788a78ce2960bbca7
Kajal <kajal@kajals-macbook-air.local>
2018-01-23 15:34:27 -0600

    Detect Horizontal Planes in the Real World

--- ARdiece/ViewController.swift
+++ ARdiece/ViewController.swift
@@ -17,17 +17,26 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+//        let sphere = SCNSphere(radius: 0.5)
+//        let material = SCNMaterial()
+//        material.diffuse.contents = UIImage(named: "art.scnassets/lisa.JPG")
+//        sphere.materials = [material]
+//        let node = SCNNode()
+//        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
+//        node.geometry = sphere
+//        sceneView.scene.rootNode.addChildNode(node)
+        sceneView.autoenablesDefaultLighting = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+//        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+//        if  let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true){
+//        diceNode.position = SCNVector3(x: 0, y: 0, z: -0.1)
+//        sceneView.scene.rootNode.addChildNode(diceNode)
+//        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,6 +44,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        
+        
+        //print(ARSessionConfiguration.isSupported)
+        print(ARWorldTrackingConfiguration.isSupported)
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -47,22 +61,24 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if anchor is ARPlaneAnchor{
+           let planeAnchor = anchor as! ARPlaneAnchor
+            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+            let planeNode = SCNNode()
+            planeNode.position = SCNVector3(x: planeAnchor.center.x, y: 0, z: planeAnchor.center.z)
+            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
+            let gridMterial = SCNMaterial()
+            gridMterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
+            plane.materials = [gridMterial]
+            planeNode.geometry = plane
+            node.addChildNode(planeNode)
+        }
+        else{
+            return
+        }
     }
-
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
+  
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
         
